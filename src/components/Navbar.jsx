5f3800640448498959a3c21aa34655be48ffdd4b
ChathuraSudaraka <Chathurabro68@gmail.com>
2023-09-17 20:21:08 +0530

    new

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,7 +64,7 @@ const Navbar = () => {
               activeClass="active"
               smooth={true}
               spy={true}
-              offset={-100}
+              offset={-50}
               to="about"
               className="block  hover:text-gray-400 py-2 px-4 cursor-pointer"
             >
