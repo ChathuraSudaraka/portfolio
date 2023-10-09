2bbd43ee90daf8faca9c56addaa9e9aba4ab4eb9
ChathuraSudaraka <Chathurabro68@gmail.com>
2023-10-09 17:35:33 +0530

    new

--- src/components/DarkLight.jsx
+++ src/components/DarkLight.jsx
@@ -5,52 +5,27 @@ import { Moon, Sun } from "./hooks/CustomTag";
 const DarkLight = () => {
   const [mode, setMode] = useThemeSwitcher();
   const [themeTransition, setThemeTransition] = useState("");
+  const [rotation, setRotation] = useState(0);
 
   const toggleTheme = () => {
     setThemeTransition("transition-all duration-500");
     setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-  };
-
-  const rotateStyle = {
-    transition: "transform 2s linear infinite",
-    animation: "rotate 2s linear infinite",
-  };
-
-  const lightButtonStyle = {
-    backgroundColor: "lightblue",
-    borderColor: "lightblue",
-    color: "black",
-    transition:
-      "background-color 0.5s ease, border-color 0.5s ease, color 0.5s ease", // Add transition properties
-  };
-
-  const darkButtonStyle = {
-    backgroundColor: "darkblue",
-    borderColor: "darkblue",
-    color: "white",
-    transition:
-      "background-color 0.5s ease, border-color 0.5s ease, color 0.5s ease", // Add transition properties
+    setRotation((prevRotation) => prevRotation + 360); // Rotate by 360 degrees
   };
 
   return (
     <div className={`bg-slate-400 ${themeTransition}`}>
-      <style>
-        {`
-          @keyframes rotate {
-            from {
-              transform: rotate(0deg);
-            }
-            to {
-              transform: rotate(360deg);
-            }
-          }
-        `}
-      </style>
       <div className="fixed top-28 right-0 w-16 bg-white rounded-l-full shadow-lg p-1">
         <button
-          className={`w-12 h-12 flex items-center justify-center rounded-full border-2 hover:bg-opacity-50 rotate-icon`}
+          className={`w-12 h-12 flex items-center justify-center rounded-full border-2 hover:bg-opacity-50`}
           onClick={toggleTheme}
-          style={mode === "dark" ? darkButtonStyle : lightButtonStyle}
+          style={{
+            backgroundColor: mode === "dark" ? "darkblue" : "lightblue",
+            borderColor: mode === "dark" ? "darkblue" : "lightblue",
+            color: mode === "dark" ? "white" : "black",
+            transition: "background-color 0.5s ease, border-color 0.5s ease, color 0.5s ease",
+            transform: `rotate(${rotation}deg)`, // Apply rotation here
+          }}
         >
           {mode === "dark" ? <Moon /> : <Sun />}
         </button>
