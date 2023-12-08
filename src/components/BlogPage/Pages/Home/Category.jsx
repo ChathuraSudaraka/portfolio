f50e8ec1986ae546299e962d527a202c86e3825a
ChathuraSudaraka <Chathurabro68@gmail.com>
2023-12-08 23:54:56 +0530

    p-2

--- src/components/BlogPage/Pages/Home/Category.jsx
+++ src/components/BlogPage/Pages/Home/Category.jsx
@@ -28,7 +28,7 @@ const Category = () => {
             selectedCategory === category
               ? "bg-gradient-to-r from-blue-500 to-blue-700 text-white"
               : "bg-gray-100 text-gray-700 hover:bg-gray-200"
-          } text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700`}
+          } text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 me-2 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700`}
         >
           {category}
         </button>
