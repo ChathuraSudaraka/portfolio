2616ce3ade68201fc2dfbb46ea04a7f29b17fa8d
ChathuraSudaraka <Chathurabro68@gmail.com>
2024-02-17 22:35:37 +0530

    tiptap success

--- src/components/BlogPage/Pages/Tiptap/NotePad.jsx
+++ src/components/BlogPage/Pages/Tiptap/NotePad.jsx
@@ -1,11 +1,11 @@
 // src/Tiptap.jsx
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import { Color } from "@tiptap/extension-color";
 import ListItem from "@tiptap/extension-list-item";
 import TextStyle from "@tiptap/extension-text-style";
-import '../Tiptap/styles.css'; // Add your custom styles if needed
+import "../Tiptap/styles.css";
 
 const extensions = [
   Color.configure({ types: [TextStyle.name, ListItem.name] }),
@@ -23,16 +23,16 @@ const extensions = [
 ];
 
 const buttonData = [
-  { command: 'toggleBold', icon: 'format_bold' },
-  { command: 'toggleItalic', icon: 'format_italic' },
-  { command: 'toggleStrike', icon: 'strikethrough_s' },
-  { command: 'toggleCode', icon: 'code' },
+  { command: "toggleBold", icon: "format_bold" },
+  { command: "toggleItalic", icon: "format_italic" },
+  { command: "toggleStrike", icon: "strikethrough_s" },
+  { command: "toggleCode", icon: "code" },
   // Add more buttons as needed
 ];
 
 const Tiptap = () => {
   const [editorContent, setEditorContent] = useState(() => {
-    const storedContent = localStorage.getItem('editorContent');
+    const storedContent = localStorage.getItem("editorContent");
     return storedContent ? storedContent : content;
   });
 
@@ -41,7 +41,7 @@ const Tiptap = () => {
     content: editorContent,
     onUpdate({ editor }) {
       const contentToSave = editor.getHTML();
-      localStorage.setItem('editorContent', contentToSave);
+      localStorage.setItem("editorContent", contentToSave);
     },
   });
 
@@ -56,21 +56,26 @@ const Tiptap = () => {
   }
 
   return (
-    <div className="mx-auto p-4">
+    <div className="mx-auto h-fit dark:bg-blog-bg p-4 pb-7">
       <div className="flex items-center space-x-2 mb-4">
         {buttonData.map((button, index) => (
           <button
             key={index}
             onClick={() => editor.chain().focus()[button.command]().run()}
             disabled={!editor.can().chain().focus()[button.command]().run()}
-            className={`editor-button ${editor.isActive(button.command) || editor.isActive(`${button.command}WithMarks`) ? "is-active" : ""}`}
+            className={`editor-button ${
+              editor.isActive(button.command) ||
+              editor.isActive(`${button.command}WithMarks`)
+                ? "is-active"
+                : ""
+            }`}
           >
             <i className="material-icons">{button.icon}</i>
           </button>
         ))}
       </div>
 
-      <div className="border p-4 rounded-md bg-white focus:outline-none">
+      <div className="border border-black dark:border-border-color rounded-lg p-4 bg-white dark:bg-gray-400 focus:outline-none">
         <EditorContent editor={editor} />
       </div>
     </div>
