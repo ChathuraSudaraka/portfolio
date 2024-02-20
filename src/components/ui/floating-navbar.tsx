4d252a7f4799a7f52498233201d4c092ac128dab
ChathuraSudaraka <Chathurabro68@gmail.com>
2024-02-20 22:03:14 +0530

    Navbar Aceternity 2nd Update

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -69,7 +69,7 @@ export const FloatingNav = ({
         ))}
         <a
           href="tel:+94705321516"
-          className="border text-sm font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 ml-2 rounded-full"
+          className="border text-sm sm:block hidden font-medium relative border-neutral-200 dark:border-white/[0.2] text-black dark:text-white px-4 py-2 ml-2 rounded-full"
         >
           <span>Contact me</span>
           <span className="absolute inset-x-0 w-1/2 mx-auto -bottom-px bg-gradient-to-r from-transparent via-blue-500 to-transparent h-px" />
