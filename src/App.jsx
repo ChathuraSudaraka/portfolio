c062e39a1eabb44ec461aa930b5773520e2e2bcb
Chathura Sudaraka <Chathurabro68@gmail.com>
2025-02-24 17:00:45 +0530

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./Pages/home";
 import BlogPage from "./Pages/Blog";
-import Work from "./Pages/Work";
 import NotePad from "./components/sections/BlogPage/Tiptap/NotePad";
 import NotFound from "./Pages/NotFound";
 import BlogData from "./Pages/BlogPage";
@@ -13,6 +12,7 @@ import CircleFollowMouse from "./hooks/use-follow-pointer";
 import CookieSettings from "./Pages/Legal/CookieSettings";
 import PrivacyPolicy from "./Pages/Legal/PrivacyPolicy";
 import TermsOfService from "./Pages/Legal/TermsOfService";
+import Work from "./Pages/work";
 
 function App() {
   return (
@@ -24,13 +24,16 @@ function App() {
           <Route path="/blog/:id" element={<BlogData />} />
           <Route path="/work" element={<Work />} />
           <Route path="/notepad/*" element={<NotePad />} />
-          <Route path="/legal/*" element={
-            <Routes>
-              <Route path="/cookie-settings" element={<CookieSettings />} />
-              <Route path="/terms-of-service" element={<TermsOfService />} />
-              <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            </Routes>
-          } />
+          <Route
+            path="/legal/*"
+            element={
+              <Routes>
+                <Route path="/cookie-settings" element={<CookieSettings />} />
+                <Route path="/terms-of-service" element={<TermsOfService />} />
+                <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+              </Routes>
+            }
+          />
           <Route path="*" element={<NotFound />} />
         </Routes>
 
